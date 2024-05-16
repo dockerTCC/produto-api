7be823667c238eed05c09c1585a370824f75ee5c
99039833 <breno.lima@sop.ce.gov.br>
2024-05-16 13:57:20 -0300

    fix: logs

--- src/main/java/multiversa/edu/produtoapi/api/controller/ProdutoController.java
+++ src/main/java/multiversa/edu/produtoapi/api/controller/ProdutoController.java
@@ -2,6 +2,7 @@ package multiversa.edu.produtoapi.api.controller;
 
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
 import multiversa.edu.produtoapi.model.dto.ProdutoDTO;
 import multiversa.edu.produtoapi.model.entity.Produto;
 import multiversa.edu.produtoapi.model.service.ProdutoService;
@@ -13,24 +14,28 @@ import java.util.List;
 @RestController
 @RequestMapping("produto")
 @RequiredArgsConstructor
+@Log
 public class ProdutoController {
 
     private final ProdutoService service;
 
     @PostMapping("cadastrar")
     public ResponseEntity<Produto> cadastrar(@RequestBody ProdutoDTO produtoDTO){
+        log.info("Cadastrando produto");
         Produto produto = service.cadastrarNovoProduto(produtoDTO);
         return ResponseEntity.ok(produto);
     }
 
     @DeleteMapping("excluir/{id}")
     public String excluir(@PathVariable Long id){
+        log.info("Excluindo produto");
         service.delete(id);
         return "Excluido com sucesso!";
     }
 
     @GetMapping
     public List<Produto> buscarTodos(){
+        log.info("Buscando todos produtos");
         return service.buscarTodos();
     }
 
