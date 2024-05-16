59a4b5b5e1fcd8cf239a78d9a94cecf4a9bb9c65
99039833 <breno.lima@sop.ce.gov.br>
2024-05-16 13:29:40 -0300

    style: ajuste

--- src/main/java/multiversa/edu/produtoapi/api/controller/ProdutoController.java
+++ src/main/java/multiversa/edu/produtoapi/api/controller/ProdutoController.java
@@ -1,10 +1,9 @@
 package multiversa.edu.produtoapi.api.controller;
 
-import jakarta.validation.Valid;
+
 import lombok.RequiredArgsConstructor;
 import multiversa.edu.produtoapi.model.dto.ProdutoDTO;
 import multiversa.edu.produtoapi.model.entity.Produto;
-import multiversa.edu.produtoapi.model.repository.ProdutoRepository;
 import multiversa.edu.produtoapi.model.service.ProdutoService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,7 +18,7 @@ public class ProdutoController {
     private final ProdutoService service;
 
     @PostMapping("cadastrar")
-    public ResponseEntity<Produto> cadastrar(@RequestBody @Valid ProdutoDTO produtoDTO){
+    public ResponseEntity<Produto> cadastrar(@RequestBody ProdutoDTO produtoDTO){
         Produto produto = service.cadastrarNovoProduto(produtoDTO);
         return ResponseEntity.ok(produto);
     }
--- src/main/java/multiversa/edu/produtoapi/model/dto/ProdutoDTO.java
+++ src/main/java/multiversa/edu/produtoapi/model/dto/ProdutoDTO.java
@@ -1,13 +1,10 @@
 package multiversa.edu.produtoapi.model.dto;
 
-import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
 
 public record ProdutoDTO(
-        @NotNull
         String nome,
-        @NotNull
         BigDecimal valor
         ) {
 }
