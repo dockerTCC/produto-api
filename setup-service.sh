296d5e80a8ecab77dcff46ebb19765d88e0c4264
breno <brenolima756@gmail.com>
2024-05-17 01:16:52 -0300

    build: config do service

---
+++ setup-service.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Variáveis
+SERVICE_NAME="produto-api"
+JAR_PATH="/home/produto-api/target/produto-api-1.0-SNAPSHOT.jar"
+USER="root"
+
+# Criar arquivo de serviço systemd
+SERVICE_FILE="/etc/systemd/system/${SERVICE_NAME}.service"
+
+echo "[Unit]
+Description=Api de cadastro produto
+After=network.target
+
+[Service]
+User=${USER}
+ExecStart=/usr/bin/java -jar ${JAR_PATH}
+SuccessExitStatus=143
+Restart=on-failure
+RestartSec=10
+
+[Install]
+WantedBy=multi-user.target" | sudo tee ${SERVICE_FILE}
+
+# Recarregar o daemon do systemd
+sudo systemctl daemon-reload
+
+# Iniciar o serviço
+sudo systemctl start ${SERVICE_NAME}.service
+
+# Habilitar o serviço para iniciar automaticamente na inicialização
+sudo systemctl enable ${SERVICE_NAME}.service
+
+# Verificar o status do serviço
+sudo systemctl status ${SERVICE_NAME}.service
+
+echo "Serviço ${SERVICE_NAME} configurado e iniciado com sucesso!"
--- setup.sh
+++ setup.sh
@@ -20,16 +20,15 @@ sudo apt install openjdk-17-jdk -y
 java -version
 
 # Instalar Maven 3.8.6
-MAVEN_VERSION=3.8.6
-wget https://downloads.apache.org/maven/maven-3/${MAVEN_VERSION}/binaries/apache-maven-${MAVEN_VERSION}-bin.tar.gz
-sudo tar xf apache-maven-${MAVEN_VERSION}-bin.tar.gz -C /opt
-sudo ln -s /opt/apache-maven-${MAVEN_VERSION} /opt/maven
-sudo ln -s /opt/maven/bin/mvn /usr/bin/mvn
+apt install maven
 
 # Verificar a instalação do Maven
-mvn -version
+mvn -v
 
-# Limpeza de arquivos temporários
-rm apache-maven-${MAVEN_VERSION}-bin.tar.gz
+# Gerar o .jar da aplicação
+mvn clean install -DskipTests
+
+#Tornar arquivo executável
+chmod +x target/produto-api-1.0-SNAPSHOT.jar
 
 echo "Instalação concluída com sucesso!"
