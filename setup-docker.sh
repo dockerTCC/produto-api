443e04b7c7decbb24dbdc84d26ac675986658150
breno <brenolima756@gmail.com>
2024-05-18 00:24:34 -0300

    build: setup do docker

---
+++ setup-docker.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Verifica se o número de instâncias foi passado como argumento
+if [ -z "$1" ]; then
+  echo "Uso: $0 <numero_de_instancias>"
+  exit 1
+fi
+
+# Número de instâncias a escalar
+NUM_INSTANCIAS=$1
+
+# Medir o tempo de início
+start_time=$(date +%s)
+
+# Comando para escalar as instâncias
+docker-compose up --scale api=$NUM_INSTANCIAS -d
+
+# Medir o tempo de fim
+end_time=$(date +%s)
+
+# Calcular e exibir a duração
+duration=$((end_time - start_time))
+echo "Tempo total para escalar com Docker Compose: $duration segundos"
