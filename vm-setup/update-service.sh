491a7013b00fe31cea9a82820199ce16f331c2e9
breno <brenolima756@gmail.com>
2024-05-22 22:52:37 -0300

    fix: update

--- vm-setup/update-service.sh
+++ vm-setup/update-service.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
-
 # Nome do serviço
 SERVICE_NAME="produto-api.service"
 # Nome do jar gerado
 JAR_NAME="produto-api-1.0-SNAPSHOT.jar"
 # Diretório onde o jar é gerado
 TARGET_DIR="../target"
-# Tempo inicial
-START_TIME=$(date +%s%3N)
 # Parar o serviço
-sudo systemctl stop $SERVICE_NAME
+#sudo systemctl stop $SERVICE_NAME
 # Fazer pull do repositório
 git pull origin main
 # Build do projeto
 mvn -f ../pom.xml clean package -DskipTests -Pvm
 chmod +x $TARGET_DIR/$JAR_NAME
+# Tempo inicial
+START_TIME=$(date +%s%3N)
 # Iniciar o serviço
-sudo systemctl start $SERVICE_NAME
+sudo systemctl restart $SERVICE_NAME
 # Aguardar até que o serviço esteja completamente iniciado
 echo "Aguardando o serviço iniciar completamente..."
 while ! sudo systemctl status $SERVICE_NAME | grep -q "Started ProdutoApiApplication"; do
