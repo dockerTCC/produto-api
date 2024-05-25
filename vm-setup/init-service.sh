251c4f8e13e50ea6da6e39d3678fabd2db8632e0
breno <brenolima756@gmail.com>
2024-05-25 18:44:12 -0300

    fix: update

--- vm-setup/init-service.sh
+++ vm-setup/init-service.sh
@@ -6,17 +6,11 @@ start_time=$(date +%s%3N)
 # Atualizar lista de pacotes
 sudo apt update
 
-# Executar arquivo .sh de instalação do PostgreSQL 9.4 e contribuições
-chmod +x install-postgres-94.sh && ./install-postgres-94.sh
-
 # Instalar JDK 17
 sudo apt install openjdk-17-jdk -y
 
 # Instalar Maven 3.8.6
 apt install maven
-
-#Istalar nginx
-chmod +x install-nginx.sh && ./install-nginx.sh
 
 # Gerar o produto-api.jar da aplicação, pulando os testes, apontando para perfil vm e Tornar arquivo .jar executável
 mvn -f ../pom.xml clean package -DskipTests -Pvm && chmod +x ../target/produto-api-1.0-SNAPSHOT.jar
